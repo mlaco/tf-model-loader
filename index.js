729ba390e912e7e211e74257da8e06dd6bcf995e
Morgan Laco <morganlaco@gmail.com>
2019-07-31 21:56:09 -0400

    Prettify

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const concatenateArrayBuffers = require(`${__dirname}/lib/utils`);
-const fs = require("fs");
-const path = require("path");
-const Buffer = require("buffer").Buffer;
+const concatenateArrayBuffers = require(`${__dirname}/lib/utils`)
+const fs = require('fs')
+const path = require('path')
+const Buffer = require('buffer').Buffer
 
 module.exports = async function(source) {
-  const modelConfig = JSON.parse(source);
-  const modelTopology = modelConfig["modelTopology"];
-  const weightsManifest = modelConfig["weightsManifest"];
+  const modelConfig = JSON.parse(source)
+  const modelTopology = modelConfig['modelTopology']
+  const weightsManifest = modelConfig['weightsManifest']
 
   // We do not allow both modelTopology and weightsManifest to be missing.
   if (modelTopology == null && weightsManifest == null) {
     throw new Error(
       `The JSON from HTTP path ${this.path} contains neither model ` +
         `topology or manifest for weights.`
-    );
+    )
   }
 
-  const shardPaths = [];
+  const shardPaths = []
   weightsManifest.forEach(weightsGroup => {
     weightsGroup.paths.forEach(path => {
-      shardPaths.push("../model/" + path);
-    });
-  });
+      shardPaths.push('../model/' + path)
+    })
+  })
 
-  const weightSpecs = [];
+  const weightSpecs = []
   for (const entry of weightsManifest) {
-    weightSpecs.push(...entry.weights);
+    weightSpecs.push(...entry.weights)
   }
 
-  const arrayBuffers = [];
+  const arrayBuffers = []
   shardPaths.forEach(filepath => {
-    const leafname = path.basename(filepath);
-    const data = fs.readFileSync(`./model/${leafname}`);
-    arrayBuffers.push(data);
-  });
-  const weightDataBuffer = concatenateArrayBuffers(arrayBuffers);
-  const weightDataString64 = Buffer.from(weightDataBuffer).toString("base64");
+    const leafname = path.basename(filepath)
+    const data = fs.readFileSync(`./model/${leafname}`)
+    arrayBuffers.push(data)
+  })
+  const weightDataBuffer = concatenateArrayBuffers(arrayBuffers)
+  const weightDataString64 = Buffer.from(weightDataBuffer).toString('base64')
 
   return JSON.stringify(`
     var modelLoader = {
       load: function() {
         const weightData = "${weightDataString64}"
-
-        //const buf = Buffer.from(weightData, "base64");
 
         const preBuf = new ArrayBuffer(weightData.length*2)
         const buf = new Uint16Array(preBuf)
@@ -62,5 +60,5 @@ module.exports = async function(source) {
         }
       }
     }
-  `);
-};
+  `)
+}
